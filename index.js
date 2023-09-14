ae2727dc948ee02a2c9ff3e8a58b3251bd4cf9f1
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-09-14 18:05:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { data } from "./data/data.js"
+import { data } from "data/data.js"
 
 const brand = document.querySelector('.brand')
 const categoryList = document.querySelector('.categorylist')
